3e57a6bc6e039a05f1bde8fb218a706c5016e832
Duncan Mac-Vicar P <dmacvicar@suse.de>
2009-07-31 00:56:23 +0200

    skeleton for test connector that will be used to test the Base class

---
+++ lib/active_cim/test_connector.rb
@@ -0,0 +1,18 @@
+
+module ActiveCim
+
+  # Connector that retrieves static data from a directory specified
+  # in site
+  #
+  #
+  class TestConnector
+
+    def initialize(site)
+      super(site)
+    end
+
+    
+    
+  end
+
+end
